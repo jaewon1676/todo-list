dc6ab44e2f1990162ded389a20357f3357e7b98b
jaewon1676 <jaewon1676@daum.net>
2021-12-21 02:08:35 +0900

    todo-app

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Todo from './Todo'
 import TodoForm from './TodoForm'
 
 
@@ -7,19 +8,29 @@ function TodoList() {
 
     const addTodo = todo => {
         if(!todo.text || /^\s*$/.test(todo.text)){
-            return
+            return;
         }
 
         const newTodos = [todo, ...todos]
 
-        setTodos(newTodos)
-        console.log(todo, ...todos);
+        setTodos(newTodos);
+    };
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id){
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        })
+        setTodos(updatedTodos);
     }
 
     return (
         <div>
             <h1>What's the Plan for Today?</h1>
             <TodoForm onSubmit={addTodo} />
+            <Todo todos={todos} completeTodo={completeTodo} />
         </div>
     )
 }
