215718af6fddca3e45db63499688b69ecc8aa289
jaewon1676 <jaewon1676@daum.net>
2021-12-22 00:37:53 +0900

    todo-app

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,27 @@
 import React, {useState} from 'react'
+import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 
-function Todo({todos, completeTodo}) {
+
+function Todo({todos, completeTodo, removeTodo, updateTodo}) {
     const [edit, setEdit] = useState({
         id: null,
         value: ''
-    })
+    });
+
+    const submitUpdate = value => {
+        updateTodo(edit.id, value)
+        setEdit({
+            id: null,
+            value: ''
+        })
+    }
+
+    if (edit.id){
+        return <TodoForm edit={edit} onSubmit={submitUpdate} />
+    }
+
+
     
     return todos.map((todo, index) => (
         <div className={todo.isComplete ? 'todo-row complete' :
@@ -13,9 +29,10 @@ function Todo({todos, completeTodo}) {
         <div key={todo.id} onClick={() => completeTodo(todo.id)}>
             {todo.text}
         </div>
-
-
-
+        <div className='icons'>
+            <span className='delete-icon' onClick={() => removeTodo(todo.id)}>x</span>
+            <span className='edit-icon' onClick={() => setEdit({id: todo.id, value: todo.text})}>수정</span>
+        </div>
     </div>
     ))
 }
